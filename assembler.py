1f447405f600639a4612e44388a4659545774429
tontae <tontaeprogrammer@gmail.com>
2019-11-22 20:58:13 +0700

    label check

--- assembler.py
+++ assembler.py
@@ -27,27 +27,37 @@ def labelcheck(filename):
         for line1 in f2:
             check = False
             label = re.split(r"\s+", line1, 5)
-            for line2 in f2:
-                check = False
-                instruc = re.split(r"\s+", line2, 5)
-                if label[4] == instruc[0]:
-                        if instruc[1] == 'halt' or instruc[1] == 'noop':
-                            check = True
-                            break
-                        elif instruc[1] != '':
-                            if instruc[2] != '':
-                                check = True
-                                break
-                            else:
-                                continue
+            # print(label)
+            if len(label) < 5:
+                # print('low')
+                continue
+            if not (label[4].lstrip('-').isdigit()):
+                with open(filename, 'r') as f3:
+                    for line2 in f3:
+                        check = False
+                        instruc = re.split(r"\s+", line2, 5)
+                        # print(instruc)
+                        if label[4] == instruc[0]:
+                                if instruc[1] == 'halt' or instruc[1] == 'noop':
+                                    check = True
+                                    break
+                                elif instruc[1] != '':
+                                    if instruc[2] != '':
+                                        check = True
+                                        break
+                                    else:
+                                        continue
+                                else:
+                                    continue
                         else:
-                            continue
-                else:
-                    continue
-            
-        if check == False:
-            raise ValueError('label undifine : ')
-    return label_addr
+                            continue        
+                if check == False:
+                    raise ValueError('label undifine')
+                # else:
+                #     print('pass')
+            else:
+                # print('number')
+                continue
 
 
 def twocompliment_16bit(num):
